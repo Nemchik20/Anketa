da625fd04256938015358d50e537e9f25ec236d2
kjk080kjk0@mail.ru <kjk080kjk0@mail.ru>
2023-09-20 23:13:32 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Привет, это анкетка на регистрацию и.т.п");
+        Console.WriteLine("Сейчас программа задаст тебе некоторые вопросы");
+        Console.WriteLine("Как ваше имя:");
+        string YourName = Console.ReadLine();
+        Console.WriteLine("Хорошо {0}! \nСледующий вопрос \nСколько вам полных лет:", YourName);
+        byte YourAge = byte.Parse(Console.ReadLine());
+        Console.WriteLine("Прекрастно! \nСледующий вопрос \nВаше любимое блюдо?");
+        string YourFood = Console.ReadLine();
+        Console.WriteLine("Невероятно {0}", YourFood);
+    }
+}
